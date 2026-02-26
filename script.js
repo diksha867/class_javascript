81ba52e05c469de4520b659bc1ecf1d521723af0
Your Name <dikshash867@gmail.com>
2026-02-26 10:28:23 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+let grade = "b"
+switch (grade) {
+    case "a":
+        console.log("excellent")
+        break;
+    case "b":
+        console.log("good")
+        break;
+    case "c":
+        console.log("average")
+        break;
+    case "d":
+        console.log("poor")
+        break;
+    default:
+        console.log("fail")
+}
