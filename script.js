087ebddf820ae12fcf84b95c0a7be728c1c4b58b
Your Name <dikshash867@gmail.com>
2026-02-28 15:18:18 +0530

    loop:for and while

--- script.js
+++ script.js
@@ -1,17 +1,36 @@
-let grade = "b"
-switch (grade) {
-    case "a":
-        console.log("excellent")
-        break;
-    case "b":
-        console.log("good")
-        break;
-    case "c":
-        console.log("average")
-        break;
-    case "d":
-        console.log("poor")
-        break;
-    default:
-        console.log("fail")
-}
+// let grade = "b"
+// switch (grade) {
+//     case "a":
+//         console.log("excellent")
+//         break;
+//     case "b":
+//         console.log("good")
+//         break;
+//     case "c":
+//         console.log("average")
+//         break;
+//     case "d":
+//         console.log("poor")
+//         break;
+//     default:
+//         console.log("fail")
+// }
+// let i;
+// for(i=1;i<=10 ;i=i+1){git 
+//    console.log(2*i)
+
+// }
+// console.log(i)
+// while(i<=10){
+//     console.log(2*i)
+//     i=i+1
+
+// while 
+// let str="hello welcome to javascript classes"
+// let i=0
+// while(str[i])
+// {
+//     if(str[i]==="v") break;
+//     console.log(str[i])
+//     i=i+1
+// }
