67385f6a46b1bfca84ef97289ff38107e9065328
Diksha Sharma <dikshash867@gmail.com>
2026-04-09 15:55:22 +0530

    updation

--- reactapp/vite-project/src/component/homePage.jsx
+++ reactapp/vite-project/src/component/homePage.jsx
@@ -2,14 +2,19 @@ import react from 'react'
 
 function HomePage(){
     const loggedin=true;
-    if(loggedin){
-        return (
-            <button>LOGOUT</button>
-        )
-    }else{
-        return(
-            <button>login</button>
-        )
-    }
+//if:else
+//     if(loggedin){
+//         return (
+//             <button>LOGOUT</button>
+//         )
+//     }else{
+//         return(
+//             <button>login</button>
+//         )
+//     }
+//ternary operaton:
+    return(
+        loggedin?<button>LOGOUT</button>:<button>LOGIN</button>
+    )
 }
 export default HomePage
