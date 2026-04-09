d67ac4ae3e6e621fded3810a90ccef81787a2335
Diksha Sharma <dikshash867@gmail.com>
2026-04-09 13:48:21 +0530

    commit

---
+++ reactapp/vite-project/src/component/Article.jsx
@@ -0,0 +1,21 @@
+import react from 'react';
+
+function Article(props) {
+    const [count, setCount] = react.useState(0);
+
+    const handleIncreamnet = () => {
+        setCount(count + 1);
+    }   
+    return (
+        <div>
+            <h1>Article</h1>
+            <h3>{props.title}</h3>
+            <p>lorem ipsum dolor sit amet</p>
+            <h2>{count}</h2>
+            <button onClick={handleIncreamnet}>Increment</button>
+            {/* <button onClick={handleDecrement}>Decrement</button> */}
+        </div>
+    )
+}
+
+export default Article;
