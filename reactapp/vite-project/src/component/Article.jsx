b3d10aeed0291d524e02f1d331f9cf340cba959e
Diksha Sharma <dikshash867@gmail.com>
2026-04-16 09:34:04 +0530

    triumph

--- reactapp/vite-project/src/component/Article.jsx
+++ reactapp/vite-project/src/component/Article.jsx
@@ -1,30 +1,92 @@
-import react from 'react';
-
+import React, {useState} from 'react'
+import style from "../css/Article.module.css"
+import style1 from "../css/HomePage.module.css"
+// import styled from "styled-components"
+import styled from "@emotion/styled"
+import { css } from '@emotion/react'
 function Article(props) {
-    const [count, setCount] = react.useState(0);
+  const [count, setCount]=useState(0)
 
-    const handleIncreamnet = () => {
-        setCount(count + 1);
-    }   
-    return (
-        <div>
-            <h1>Article</h1>
-            <h3>{props.title}</h3>
-            <p>lorem ipsum dolor sit amet</p>
-            <h2>{count}</h2>
-            <button onClick={handleIncreamnet}>Increment</button>
-            {/* <button onClick={handleDecrement}>Decrement</button> */}
-        </div>   
-    )
+
+  const handleIncrement=()=>{
+    setCount(count+1)
+  }
+  return (
+    <div>
+      <h1>Article</h1>
+       <h3>{props.title}</h3> 
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+      <h2>{count}</h2>
+      <button className={style.btn} onClick={handleIncrement}>Increment</button>
+      <button css={{backgroundColor:"green", color:"white"}}> First</button>
+      <Button>Submit</Button>
+      <Button yellow >Click Me!</Button>
+    </div>
+  )
 }
 
-export default Article;
+export default Article
 
-const button=styled.button`
-background-color:${props=>props.blue?'blue':'gray'};
-color:white;
-padding:10px;
-border-radius:5px;
+const Button=styled.button`
+background-color: ${props=>props.yellow ? "yellow" : "red"};  
+color: white;
+width: 100px;
+height: 30px;
 `
 
+// import React from 'react'
+// import { add as addition } from '../utility'
 
+// class Article extends React.Component {
+//     constructor(props) {
+//       super(props)
+//       this.state={
+//         count:0,
+//         name:"Alex"
+//       }
+//     //   console.log(addition(2,3))
+//     }
+
+//     componentDidMount(){
+//         console.log("componentDidMount")
+//     }
+
+//     shouldComponentUpdate(nextProps, nextState){
+//       if(nextState.count===1)return false
+//         console.log("shouldComponentUpdate")
+//         return true
+//     }
+
+
+//     componentDidUpdate(){
+//         console.log("componentDidUpdate")
+//     }
+
+//     componentWillUnmount(){
+//         console.log("componentWillUnmount")
+//     }
+
+//     handleClick=()=>{
+//         // this.state.count=this.state.count+1
+//         // console.log(this.state.count)
+//         this.setState({count: this.state.count+1})
+//     }
+
+//     handleDecrement=()=>{
+//         this.setState({count: this.state.count-1})
+//     }
+//   render() {
+//     return (
+//       <div>
+//         <h1>Article</h1>
+//         <h3>{this.state.count}</h3>
+//         <button onClick={this.handleClick}>Increment</button>
+//         <button onClick={this.handleDecrement}>Decrement</button>
+//         <h3>{this.props.title}</h3> 
+//         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi eligendi fugiat pariatur eos quibusdam ratione possimus adipisci, impedit, autem ad quo, et cum laudantium deleniti. Soluta molestias amet quas labore.</p>
+//       </div>
+//     )
+//   }
+// }
+
+// export default  Article
